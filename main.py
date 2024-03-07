0989cc02dc93d346776d6c2bdece3978c9f1165c
AbiyuTamirat2 <148640715+AbiyuTamirat2@users.noreply.github.com>
2024-03-06 20:36:50 -0800

    Add Python script to fetch and insert car data into MySQL database

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-# CNE 340
-# Project Name Car
+# CNE 340 Winter quarter 2024
+# Abiyu and Scott
+
+
+import mysql.connector
+import json
+import requests
+
+
+def connect_to_sql():
+    connection = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='cars_database')
+
+    return connection
+
+
+# Create a cars table if it doesn't exist
+def create_table(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS cars (model VARCHAR(255), mpg FLOAT, cyl INT, disp FLOAT, hp INT, 
+    drat FLOAT, wt FLOAT, qsec FLOAT, vs INT, am INT, gear INT, carb INT)''')
+
+
+# Insert a new car record into the cars table
+def add_new_car(cursor):
+    cursor.execute('''INSERT INTO cars(model, mpg, cyl, disp, hp, drat, wt, qsec, vs, am, gear, carb) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''')
+
+
+def fetch_cars():
+    response = requests.get('https://gist.github.com/noamross/e5d3e859aa0c794be10b')
+    data = json.loads(response.text)
+    return data
+
+
+def main():
+    conn = connect_to_sql()
+    cursor = conn.cursor()
+    create_table(cursor)
+
+
+if __name__ == '__main__':
+    main()
+
